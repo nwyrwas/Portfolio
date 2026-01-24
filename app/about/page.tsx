fd358cb3fc153166b1e7f6348e32a81129f785a3
Nick Wyrwas <nick.wyrwas@outlook.com>
2026-01-23 21:44:55 -0600

    Changed contact info

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -94,7 +94,7 @@ export default function AboutPage() {
                       <svg className="w-5 h-5 text-gray-400 group-hover:text-primary-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                       </svg>
-                      <span className="break-all">nick.wyrwas@gmail.com</span>
+                      <span className="break-all">nick.wyrwas@outlook.com</span>
                     </a>
                     <a
                       href="https://linkedin.com/in/nicholas-wyrwas"
